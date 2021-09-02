cb28d02570bed77948f36b915549740d982ebb30
npathirana1 <nethmi.pathirana@gmail.com>
2021-09-02 16:05:35 +0530

    style changes to form

--- src/views/Receptionist/updateCustomer.php
+++ src/views/Receptionist/updateCustomer.php
@@ -7,6 +7,7 @@
 </head>
 
 <body>
+<center>
 
 <div class="form_box">
 
@@ -15,23 +16,18 @@
   <div class="form_body">
 
   <br><div class="form_title">Update Profile details</div>
+  <hr>
 
-  <form>
-
-  <label for="first_name"> &nbsp;<div class="form_label" style="float:left;" >First name:</div></label>
   <input type="text" placeholder="Enter Your first name" name="first_name" id="first_name"required><br>
 
-  <label for="last_name"> &nbsp;<div class="form_label" style="float:left;">Last name:</div></label>
   <input type="text" placeholder="Enter Your last name" name="last_name" id="last_name"required><br>
   
-  <label for="e-mail"> &nbsp;<div class="form_label" style="float:left;">Email Address:</div></label>
-  <input type="email" placeholder="Enter Your email address" name="e-mail" id="e-mail"required><br>
+  <input type="text" placeholder="Enter Your email address" name="e-mail" id="e-mail"required><br>
 
-  <label for="mobile_tel_no">&nbsp; <div class="form_label" style="float:left;">Mobile Number:</div></label>
   <input type="text" placeholder="Enter Your mobile number" name="mobile_tel_no" id="mobile_tel_no"required><br>
 
-  <label for="psword"> &nbsp;<div class="form_label" style="float:left;">Password</div></label>
-  <input type="password" name="psword" id="psword" placeholder="password" required><br>
+  <input type="password" name="psword" id="psword" placeholder="Password" required><br>
+  <hr>
 
   <button type="submit" class="form_btn">Update</button>
 
@@ -39,7 +35,8 @@
 </form> 
 
 </div>
-    
+ 
+</center>
 </body>
 
 </html>
