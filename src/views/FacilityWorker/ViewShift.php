898567be416dbefbb5a3398a5950162568c118e3
DamithaD <damithadwickramasinghe@gmail.com>
2021-09-06 18:27:20 +0530

    viewShift

---
+++ src/views/FacilityWorker/ViewShift.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/viewShift.css"> 
+    </head>
+
+    <body>
+        <div class="form_box">    
+            <form action="/action_page.php">
+            <div class="form_body">
+                <center>
+                    <p class="form_title"> Work shedule </p> <hr>
+                </center>
+                <br>
+                <label for="ldate">Date</label>
+
+                <br><br>
+                <label for="=Assigned_fac">Assigned facility</label> 
+                <input type="text" id="Assigned_fac" name="Assigned facility">
+                <br><br>
+                <label for="shift">Shift</label>
+                <input type="text" id="shift" name="Shift">
+                <hr>
+            </div>
+            </form>
+         </div>
+
+    </body>
