37f7a599dc4d596e20bbcbeb55534e3117bd447a
npathirana1 <nethmi.pathirana@gmail.com>
2021-09-23 21:51:48 +0530

    minor update

--- src/views/Customer/customerIncludes/registration.inc.php
+++ src/views/Customer/customerIncludes/registration.inc.php
@@ -23,15 +23,15 @@
         elseif (isset($_POST['FName']) && isset($_POST['LName']) && isset($_POST['Email']) && isset($_POST['TelephoneNo']) && isset($_POST['NIC']) && isset($_POST['NIC']) && isset($_POST['UserPsword'])) {
             if (!empty($_POST['FName']) && !empty($_POST['LName']) && !empty($_POST['Email']) && !empty($_POST['TelephoneNo']) && !empty($_POST['NIC']) && !empty($_POST['NIC']) && !empty($_POST['UserPsword'])) {
                 
-                //$hashedPwd = password_hash($UserPsword, PASSWORD_DEFAULT);
+                $hashedPwd = password_hash($UserPsword, PASSWORD_DEFAULT);
 
-                $query = "INSERT INTO customer (FName,LName,Email,TelephoneNo,NIC,UserPsword) VALUES ('$FName','$LName','$Email','$TelephoneNo','$NIC','$UserPsword')";
+                $query = "INSERT INTO customer (FName,LName,Email,TelephoneNo,NIC,UserPsword) VALUES ('$FName','$LName','$Email','$TelephoneNo','$NIC','$hashedPwd')";
                 
                 $result = mysqli_query($conn, $query);
 
                 if ($result) {
 
-                    $login_query = "INSERT INTO user_login (Email, UserPsword, UserType) VALUES ('$Email','$UserPsword','$UserType')"; 
+                    $login_query = "INSERT INTO user_login (Email, UserPsword, UserType) VALUES ('$Email','$hashedPwd','$UserType')"; 
                     $result1 = mysqli_query($conn, $login_query);
 
                     echo "<script>
