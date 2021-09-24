fdd3dc2b3931cc75940ddc9a1e303cd3bcaf70c0
Sandali <2019is015@stu.ucsc.cmb.lk>
2021-09-24 09:58:49 +0530

    Navigation of manager, logo and the icon

---
+++ src/views/Manager/managerIncludes/ManagerNavigation.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+<link rel="stylesheet" type="text/css" href="../../assets/CSS/style5.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="shortcut icon" href="../../assets/Images/icon.png" type="image/x-icon"> 
+
+<style>
+      /*font sixe is changed from 18->15*/
+      .sidenav a {
+         font-size: 15px; 
+      }
+</style>
+</head>
+
+<body>
+
+<div class="sidenav">
+   
+     <img src="../../assets/Images/logo.png" alt="Logo">
+    
+      <p class="side_nav_title"> User name </p>     
+      <a href="managerIndex.php"><i class="fa fa-fw fa-home"></i>Dashboard</a>
+      <a href="#reservation"><i class="fa fa-calculator"></i>Reservation</a>
+      <a href="#customer"><i class="fa fa-user-circle-o"></i>Customers</a>
+      <a href="#employee"><i class="fa fa-user-circle-o"></i>Employees</a>
+      <a href="#shifts"><i class="fa fa-fw fa-user-circle-o"></i>Shifts</a>
+      <a href="#leaves"><i class="fa fa-fw fa-file"></i>Leaves</a>
+      <a href="#reports"><i class="fa fa-file-text"></i>Reports</a>
+      <a href="#rinquiries"><i class="fa fa-location-arrow"></i>Inquiries</a>
+      <a href="#facilityschedule"><i class="fa fa-calendar"></i>Facility Schedule</a>
+      
+      <div></div>
+      <a href="#logout"><i class="fa fa-fw fa-sliders"></i>Log out</a>
+      </div>
+</div>
+   
+</body>
+</html> 
