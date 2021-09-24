c9403108991e793b7f9c9168e4b217d51e88f862
npathirana1 <nethmi.pathirana@gmail.com>
2021-09-24 09:50:01 +0530

    Manager navbar

---
+++ src/views/Receptionist/receptionistIncludes/sideNav.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
+<link rel="stylesheet" type="text/css" href="../../../assets/CSS/navBar.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<div class="sidenav">
+   
+     <img src="../../../assets/Images/logo.png" alt="Logo" >
+    
+      <p class="side_nav_title"> User name </p>     
+      <a href="#homescreen"><i class="fa fa-fw fa-home"></i>DashBoard</a>
+      <a href="#"><i class="fa fa-fw fa-home"></i>Reservations</a>
+      <a href="#leaves"><i class="fa fa-fw fa-file"></i>Customers</a>
+      <a href="#shifts"><i class="fa fa-fw fa-user-circle-o"></i>Shifts</a>
+      <a href="#leaves"><i class="fa fa-fw fa-file"></i>Leaves</a>
+      <a href="#leaves"><i class="fa fa-fw fa-file"></i>Inquiries</a>
+      <a href="#leaves"><i class="fa fa-fw fa-file"></i>Facility Schedule</a>
+
+      
+      <a href="#logout"><i class="fa fa-fw fa-sliders"></i>Log out</a>
+
+</div>
+
+   
+</body>
+</html> 
