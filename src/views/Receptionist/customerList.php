fd630b92444a9928b46ad3432bf36d56d4b495a2
npathirana1 <nethmi.pathirana@gmail.com>
2021-09-26 15:40:16 +0530

    Receptionist dashboard

--- src/views/Receptionist/customerList.php
+++ src/views/Receptionist/customerList.php
@@ -6,18 +6,19 @@
     </head>
 
     <body>
-    
-    <h2 class="table_topic">Registered Customers</h2>
 
-    <table class="table_view">
+    <?php include "./receptionistIncludes/receptionistNavigation.php"; ?>
+
+    <section class="home-section-table">
+    <h2 class="table_topic">Registered Customers</h2>
+                <table class="table_view">
                     <thead>
                         <tr>
                             <th>Customer ID</th>
                             <th>Customer Name</th>
                             <th>Contact Number</th>
                             <th>Email</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
+                            <th>View</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -27,17 +28,16 @@
                             <td>88,110</td>
                             <td>jdswml</td>
                             <td></td>
-                            <td></td>
                         </tr>
                         <tr>
                             <td>2</td>
                             <td>Sally</td>
                             <td>72,400</td>
-                            <td>Stweowkudents</td>
-                            <td></td>
+                            <td>nethmi.pathirana@gmail.com</td>
                             <td></td>
                         </tr>
                     </tbody>
-                    </table> 
+                </table> 
+    </section>
     </body>
 </html>
--- src/views/Receptionist/receptionistIndex.php
+++ src/views/Receptionist/receptionistIndex.php
@@ -1,72 +1,177 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>
-            Home
-        </title>
-        <link rel="stylesheet" type="text/css" href="../../assets/CSS/home.css">
-    </head>
-    <body>
-        <div>
-        <?php include "./receptionistIncludes/receptionistNavigation.php"; ?>
-        </div>
-        <div class="main">
-            
-            <div class="defaultbg" id="notification">
-                <p class="topic">Notification</p>
-                <!-- repeat the div display class for each notification-->
-                <div class="display"><!-- a single notification is written should improve this to dispaly multiple. recursive is better-->
-                    <p style="padding: left 25px;">Date &emsp; Time</p>
-                    <p>Notification</p>
-                </div>
-                <!--this should be removed after completing the backend-->
-                <div class="display"><!-- a single notification is written should improve this to dispaly multiple. recursive is better-->
-                    <p style="padding: left 25px;">Date &emsp; Time</p>
-                    <p>Notification</p>
-                </div>
-                <div style=" padding-bottom: 20px;">
-                </div>
-            </div>
+  <head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="../../assets/CSS/dashboard.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/CSS/viewTables.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  </head>
+  <body>
+    
+  <?php include "./receptionistIncludes/receptionistNavigation.php"; ?>
+     
+  <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Dashboard</span>
+      </div>
+        <!--<img src="images/profile.jpg" alt="">-->
+        <span class="admin_name">Receptionist</span>
+        <i class='bx bx-chevron-down' ></i>
+      </div>
+    </nav>
 
-            <div class="grid">
-                <div class="defaultbg list" id="left_pannel"> <!-- most booked facilities-->
-                    <p class="topic">
-                        Most Booked Facilities
-                    </p>
-                    <div>
-                        <!-- incluse the most booked facilities-->
-                       <p>Facility 1</p>
-                        <p>Facility 2</p>
-                        <p>Facility 3</p>
-                    </div>      
-                </div>
-
-                <div class="defaultbg list" id="right_pannel"> <!-- top customers-->
-                    <p class="topic">
-                        Most Booked Facilities
-                    </p>
-                    <div>
-                        <!-- incluse the top customers-->
-                        <p>Customer 1</p>
-                        <p>Customer 2</p>
-                        <p>Customer 3</p>
-                    </div> 
-                </div>
+    <div class="home-content">
+      <div class="overview-boxes">
+        <div class="box">
+          <div class="right-side">
+          <div class="box-topic">This weeks reservations</div>
+              <div class="number">40,876</div>
+              <div class="indicator">
+                <i class='bx bx-up-arrow-alt'></i>
+                <span class="text">Up from last week</span>
+              </div>
             </div>
-            <div class="reoprt"><!--for the 4 squares with the reports-->
-                <div class="single_report" style="float: left;">
-                    report1
-                </div>
-                <div class="single_report">
-                    report2
-                </div>
-                <div class="single_report">
-                    report3
-                </div>
-                <div class="single_report">
-                    report4
-                </div>
-            </div>
+            <i class='bx bxs-cart-alt cart reservation'></i>
         </div>
-    </body>
+        <div class="box">
+          <div class="right-side">
+          <div class="box-topic">New customer registrations</div>
+              <div class="number">38,876</div>
+              <div class="indicator">
+                <i class='bx bx-up-arrow-alt'></i>
+                <span class="text">Up from last week</span>
+              </div>
+            </div>
+            <i class='bx bxs-user-plus cart user'></i>
+        </div>
+        <div class="box">
+          <div class="right-side">
+          <div class="box-topic">Revenue for the past month</div>
+              <div class="number">$12,876</div>
+              <div class="indicator">
+                <i class='bx bx-down-arrow-alt down'></i>
+                <span class="text">Down from last month</span>
+              </div>
+            </div>
+            <i class='bx bx-money cart revenue'></i>
+        </div>
+        <div class="box">
+          <div class="right-side">
+          <div class="box-topic">Total Inquiries Received</div>
+              <div class="number">11,086</div>
+            </div>
+            <i class='bx bxs-bell-plus cart inquiry'></i>
+        </div>
+      </div>
+
+      <div class="sales-boxes">
+        <div class="recent-sales box">
+          <div class="title">Recent Sales</div>
+          <div class="sales-details">
+          <table class="table_view">
+                    <thead>
+                        <tr>
+                            <th>Reservation ID</th>
+                            <th>Customer Name</th>
+                            <th>Contact Number</th>
+                            <th>Facility</th>
+                            <th>View</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Domenic</td>
+                            <td>88,110</td>
+                            <td>jdswml</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Sally</td>
+                            <td>72,400</td>
+                            <td>Stweowkudents</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                    </table> 
+          </div>
+          <div class="button">
+            <a href="#">See All</a>
+          </div>
+        </div>
+        <div class="top-sales box">
+          <div class="title">Top Seling Product</div>
+          <ul class="top-sales-details">
+            <li>
+            <a href="#">
+              <!--<img src="images/sunglasses.jpg" alt="">-->
+              <span class="product">Vuitton Sunglasses</span>
+            </a>
+            <span class="price">$1107</span>
+          </li>
+          <li>
+            <a href="#">
+               <!--<img src="images/jeans.jpg" alt="">-->
+              <span class="product">Hourglass Jeans </span>
+            </a>
+            <span class="price">$1567</span>
+          </li>
+          <li>
+            <a href="#">
+             <!-- <img src="images/nike.jpg" alt="">-->
+              <span class="product">Nike Sport Shoe</span>
+            </a>
+            <span class="price">$1234</span>
+          </li>
+          <li>
+            <a href="#">
+              <!--<img src="images/scarves.jpg" alt="">-->
+              <span class="product">Hermes Silk Scarves.</span>
+            </a>
+            <span class="price">$2312</span>
+          </li>
+          <li>
+            <a href="#">
+              <!--<img src="images/blueBag.jpg" alt="">-->
+              <span class="product">Succi Ladies Bag</span>
+            </a>
+            <span class="price">$1456</span>
+          </li>
+          <li>
+            <a href="#">
+              <!--<img src="images/bag.jpg" alt="">-->
+              <span class="product">Gucci Womens's Bags</span>
+            </a>
+            <span class="price">$2345</span>
+          <li>
+            <a href="#">
+              <!--<img src="images/addidas.jpg" alt="">-->
+              <span class="product">Addidas Running Shoe</span>
+            </a>
+            <span class="price">$2345</span>
+          </li>
+          <li>
+            <a href="#">
+             <!--<img src="images/shirt.jpg" alt="">-->
+              <span class="product">Bilack Wear's Shirt</span>
+            </a>
+            <span class="price">$1245</span>
+          </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+     
+</body>
 </html>
