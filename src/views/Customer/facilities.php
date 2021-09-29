992facad508f0c4dab757f1f1552a8523399547f
AshaneKG <2019is028@stu.ucsc.lk>
2021-09-29 15:10:34 +0530

    nav

--- src/views/Customer/facilities.php
+++ src/views/Customer/facilities.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="../assets/CSS/facilties.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/CSS/facilties.css">
 </head>
 <body>
 
