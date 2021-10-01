a16a038e8ec84abb5823e2f9c15a0755978e5f04
AshaneKG <2019is028@stu.ucsc.lk>
2021-10-01 10:56:09 +0530

    update

--- src/views/Customer/facilities.php
+++ src/views/Customer/facilities.php
@@ -4,22 +4,47 @@
   <link rel="stylesheet" type="text/css" href="../../assets/CSS/facilities.css">
 </head>
 <body>
-
+<center><table> <tr> <td>
 <div class="card" >
   <img src="../../assets/Images/fbasketball.png" alt="Basketball Court" style="width:100%">
   <h1>Basketball Court</h1>
   <p class="price">1000LKR/Hour</p>
   <p>Outwit your opponent on our basketball courts and create opportunities to score!</p>
   <button class="button"><span>Book now</span></button>
-</div> 
- <br> <br> <br>
+</div> </td> 
+
+ <td>
 <div class="card">
     <img src="../../assets/Images/fbadminton.png" alt="Badminton Court" style="width:100%">
     <h1>Badminton Courts</h1>
     <p class="price">500LKR/Hour</p>
-    <p>Whether you’re booking a court as a group, competing against others or want to learn how to play, FlexSports has a range of badminton options for you to enjoy at your local leisure centre.</p>
+    <p>Push yourself to your limits by dueling your friends in badminton.!</p>
     <button class="button"><span>Book now</span></button>
   </div>
+</td>
+<td>
+<div class="card">
+    <img src="../../assets/Images/fbilliard.png" alt="Billiard table" style="width:100%">
+    <h1>Billiard tables</h1>
+    <p class="price">500LKR/Hour</p>
+    <p>Push yourself to your limits by dueling your friends in billiard.!</p>
+    <button class="button"><span>Book now</span></button>
+  </div>
+</td>
+</tr>
+<tr> 
+<td>
+<div class="card">
+    <img src="../../assets/Images/ftabletennis.png" alt="Table Tennis" style="width:100%">
+    <h1>Table Tennis</h1>
+    <p class="price">500LKR/Hour</p>
+    <p>Push yourself to your limits by dueling your friends in Table Tennis.!</p>
+    <button class="button"><span>Book now</span></button>
+  </div>
+</td>
+</tr>
+
+</table></center>
   <p> billiard tabletennis volleyball</p>
 
 </body>
