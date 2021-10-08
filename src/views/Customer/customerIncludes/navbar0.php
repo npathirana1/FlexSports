4c780a3102f8ed69ae6b2e3e7844869c0146295f
AshaneKG <2019is028@stu.ucsc.lk>
2021-10-08 23:50:20 +0530

    updates

---
+++ src/views/Customer/customerIncludes/navbar0.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>landing page</title>
+    <link rel="stylesheet" href="../../assets/CSS/nav.css">
+</head>
+<body>
+    <section>
+        
+        <header>
+            <a href="#"><img src="../../assets/Images/logo.png" class="logo"></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="aboutus.php">About us</a></li>
+                <li><a href="facilities.php">Facilities</a></li>
+                <li><a href="contactus.php">Contact us</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </header>
+    </section>
+</body>
+</html>
