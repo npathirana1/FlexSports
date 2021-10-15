fc0358a3433014ab3f31d158e16828cb762a1d86
npathirana1 <nethmi.pathirana@gmail.com>
2021-10-15 20:21:48 +0530

    add customer - minor update

--- src/views/Receptionist/receptionistIncludes/addCustomer.inc.php
+++ src/views/Receptionist/receptionistIncludes/addCustomer.inc.php
@@ -21,60 +21,64 @@ if (isset($_POST['submit'])) {
         $sql2 = "SELECT * FROM customer WHERE NIC='$NIC'";
         $user1 = mysqli_query($conn, $sql2);
 
-        //check for user
-        if (mysqli_num_rows($user) > 0) {
-            echo
-            "<script>
+
+        if ($UserPsword === $repeat) {
+            //check for user
+            if (mysqli_num_rows($user) > 0) {
+                echo
+                "<script>
                 alert('User already exists');
                 window.location.href='../addCustomer.php';
             </script>";
-        }
-        //check for user with same NIC
-        elseif (mysqli_num_rows($user1) > 0) {
-            echo
-            "<script>
+            }
+            //check for user with same NIC
+            elseif (mysqli_num_rows($user1) > 0) {
+                echo
+                "<script>
                 alert('Account already exists with this NIC');
                 window.location.href='../addCustomer.php';
             </script>";
-        }
-        //Validate email address
-        elseif (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
-            echo
-            "<script>
+            }
+            //Validate email address
+            elseif (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+                echo
+                "<script>
                 alert('Enter a valid email address');
                 window.location.href='../addCustomer.php';
             </script>";
-        }
-        //confirm password-not working
-        elseif ( $_POST['UserPsword'] != $_POST['UserPsword-repeat']) {
-            echo
-            "<script>
-                alert('Passwords don't match');
-                window.location.href='../addCustomer.php';
-            </script>";
-        }
-        //create account
-        else {
-            $hashedPwd = password_hash($UserPsword, PASSWORD_DEFAULT);
+            }
+            //confirm password-not working
 
-            $login_query = "INSERT INTO user_login (Email, UserPsword, UserType) VALUES ('$Email','$hashedPwd','$UserType')";
-            $result1 = mysqli_query($conn, $login_query);
+            //create account
+            else {
+                $hashedPwd = password_hash($UserPsword, PASSWORD_DEFAULT);
 
-            if ($result1) {
+                $login_query = "INSERT INTO user_login (Email, UserPsword, UserType) VALUES ('$Email','$hashedPwd','$UserType')";
+                $result1 = mysqli_query($conn, $login_query);
 
-                $query = "INSERT INTO customer (FName,LName,Email,TelephoneNo,NIC,UserPsword) VALUES ('$FName','$LName','$Email','$TelephoneNo','$NIC','$hashedPwd')";
-                $result = mysqli_query($conn, $query);
+                $UserID = mysqli_insert_id($conn);
 
-                if ($result) {
-                    echo "<script>
+                if ($result1) {
+
+                    $query = "INSERT INTO customer (FName,LName,Email,TelephoneNo,NIC,UserPsword,UserID) VALUES ('$FName','$LName','$Email','$TelephoneNo','$NIC','$hashedPwd','$UserID')";
+                    $result = mysqli_query($conn, $query);
+
+                    if ($result) {
+                        echo "<script>
                             alert('Customer account has been successfully created');
                             window.location.href='../customerList.php';
                         </script>";
-                } else {
-                    echo "<script>alert('failed');</script>";
-                    echo "<script>window.location.href = '../addCustomer.php';</script>";
+                    } else {
+                        echo "<script>alert('failed');</script>";
+                        echo "<script>window.location.href = '../addCustomer.php';</script>";
+                    }
                 }
             }
+        } else {
+            echo "<script type='text/javascript'>
+                alert('Password deos not match');
+                window.history.back();           
+                </script>";
         }
     } else {
         echo
@@ -84,3 +88,5 @@ if (isset($_POST['submit'])) {
         </script>";
     }
 }
+
+mysqli_close($conn);
