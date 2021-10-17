cb8cd43af496535084abaa17bd13044c7280be68
DamithaD <damithadwickramasinghe@gmail.com>
2021-10-17 11:59:02 +0530

    loggin facility worker

--- src/config/login.inc.php
+++ src/config/login.inc.php
@@ -1,6 +1,6 @@
 <?php
 include "db.php";
-session_start();
+//session_start();
 
 if (isset($_POST['login-submit'])) {
 
@@ -31,23 +31,30 @@ if (isset($_POST['login-submit'])) {
                 if ($UserType == 'customer') {
                     unset($_SESSION['receptionistID']);
                     unset($_SESSION['managerID']);
-
+                    unset($_SESSION['facilityworkerID']);
                     $_SESSION['customerID'] = $Email;
                     header('Location: ../views/Customer/profile.php');
-         
                     exit();
                 } elseif ($UserType == 'receptionist') {
                     unset($_SESSION['customerID']);
                     unset($_SESSION['managerID']);
+                    unset($_SESSION['facilityworkerID']);
                     $_SESSION['receptionistID'] = $Email;
                     header('Location: ../views/Receptionist/receptionistIndex.php');
                     exit();
-                }
-                elseif ($UserType == 'manager') {
+                } elseif ($UserType == 'manager') {
                     unset($_SESSION['customerID']);
                     unset($_SESSION['receptionistID']);
+                    unset($_SESSION['facilityworkerID']);
                     $_SESSION['managerID'] = $Email;
                     header('Location: ../views/Manager/managerIndex.php');
+                    exit();
+                } elseif ($UserType == 'facilityworker') {
+                    unset($_SESSION['customerID']);
+                    unset($_SESSION['receptionistID']);
+                    unset($_SESSION['managerID']);
+                    $_SESSION['facilityworkerID'] = $Email;
+                    header('Location: ../views/FacilityWorker/facilityWorkerDashboard.php');
                     exit();
                 }
             }
