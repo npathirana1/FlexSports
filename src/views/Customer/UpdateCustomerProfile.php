85e2e633a66b180f2077ca9bf8b1134cf98417a1
AshaneKG <2019is028@stu.ucsc.lk>
2021-12-10 23:00:43 +0530

    test

--- src/views/Customer/UpdateCustomerProfile.php
+++ src/views/Customer/UpdateCustomerProfile.php
@@ -43,25 +43,127 @@ if (isset($_SESSION['customerID'])) {
     overflow: hidden;
     border-radius: 60%;
 }
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body{
+    height: 100vh;
+    width: 100%;
+}
+
+h1{
+    font-family: sans-serif;
+    text-align: center;
+    font-size: 30px;
+    color: #222;
+}
+
+.profile-pic-div{
+    height: 200px;
+    width: 200px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    border-radius: 50%;
+    overflow: hidden;
+    border: 1px solid grey;
+}
+
+#photo{
+    height: 100%;
+    width: 100%;
+}
+
+#file{
+    display: none;
+}
+
+#uploadBtn{
+    height: 40px;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+    background: rgba(0, 0, 0, 0.7);
+    color: wheat;
+    line-height: 30px;
+    font-family: sans-serif;
+    font-size: 15px;
+    cursor: pointer;
+    display: none;
+}
 </style>
+
   
 </head>
 
 <body>
   
-<div class="circle"></div>
+<!-- <div class="circle"></div> -->
   <section style="margin-top: 300px; margin-left:-580px;" class="home-section">
   
 
   
     </br></br></br></br>
-    <form  action="#" method="post" style="min-height: 670px; margin-left:300px;min-width:400px;" class="signup-form">
+    <form  action="#" method="post" style="min-height: 670px; margin-left:300px;min-width:400px; min-width: 550px;" class="signup-form">
+    
 
       <div class="form-header">
         <h1 class="form_title">Update Profile Details</h1>
       </div>
 
-      <div style="height: 600px;" class="form-body">
+      <div style="margin-top:-170px; margin-left:774px;" class="profile-pic-div">
+  <img src="../../assets/Images/updateprofile.png" id="photo">
+  <input type="file" id="file">
+  <label for="file" id="uploadBtn">Choose Photo</label>
+</div>
+
+<script>
+
+const imgDiv = document.querySelector('.profile-pic-div');
+const img = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.style.display = "block";
+});
+
+
+
+imgDiv.addEventListener('mouseleave', function(){
+    uploadBtn.style.display = "none";
+});
+
+
+
+file.addEventListener('change', function(){
+    
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+
+        const reader = new FileReader(); 
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+
+       
+    }
+});</script>
+ 
+     <div style="height: 600px;" class="form-body">
         <div class="horizontal-group">
         <div class="form-group">
             <label for="">NIC</label> : 991162780V
@@ -70,11 +172,11 @@ if (isset($_SESSION['customerID'])) {
             <label for="">Email</label> 
             <input type="text" placeholder="Ashanegunarathne@gmail.com" name="Email" class="form-control">
           </div>  
-          <div class="form-group">
+          <div class="form-group left">
             <label for="">First Name</label>
             <input type="text" placeholder="Ashane" name="FName" class="form-control">
           </div>
-          <div class="form-group">
+          <div class="form-group right">
             <label for="">Last Name</label>
             <input type="text" placeholder="Gunarathne" name="LName" class="form-control">
           </div>
@@ -82,10 +184,10 @@ if (isset($_SESSION['customerID'])) {
           <label for="">Mobile Number</label>
           <input type="text" placeholder="0774145153" name="TelephoneNo" class="form-control">
         </div>
-        <div class="form-group">
+        <!-- <div class="form-group">
           <label for="">Password</label>
           <input type="password" placeholder="********" name="UserPsword" class="form-control">
-        </div>
+        </div> -->
         
         
 </div> 
@@ -101,8 +203,8 @@ if (isset($_SESSION['customerID'])) {
       </div>
 
     </form>
-    <div style="margin-left: 150px;" class="imgBox">
-                <img src="../../assets/Images/updatecustomerprofile.png" class="flexsports" >
+    <div style="margin-left: 10px;" class="imgBox">
+                <img src="../../assets/Images/updateprofile.png" class="flexsports" >
             </div>
 
 
