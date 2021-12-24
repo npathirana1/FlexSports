d7798fd5c7adc955167497bf08815ede3cb1d5b6
npathirana1 <nethmi.pathirana@gmail.com>
2021-12-24 18:14:38 +0530

    Inquiry CSS fix

--- src/views/Receptionist/inquiryList.php
+++ src/views/Receptionist/inquiryList.php
@@ -3,88 +3,69 @@
 
 <head>
     <title>Inquiries</title>
-    <link rel="stylesheet" type="text/css" href="../../assets/CSS/viewTables.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/CSS/staffMain.css">
     <link rel="stylesheet" type="text/css" href="../../assets/CSS/receptionistInquiry.css">
-<style>
-    .home-section-table .breadcrumb-nav {
-            display: flex;
-            justify-content: space-between;
-            height: 80px;
-            background: #fff;
-            align-items: center;
-            position: fixed;
-            width: calc(100% - 240px);
-            left: 240px;
-            z-index: 100;
-            padding: 0 20px;
-            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-            transition: all 0.5s ease;
-        }
-
-       
-
-        .home-section-table .content{
-            padding-top: 10%;
+    <style>
+        .home-section .home-content {
+            padding-top: 8%;
             position: relative;
         }
-</style>
-    
-</head> 
+    </style>
+
+</head>
 
 <body>
 
     <?php include "./receptionistIncludes/receptionistNavigation.php"; ?>
 
-    <section class="home-section-table">
-    <nav class="breadcrumb-nav">
+    <section class="home-section">
+        <nav class="breadcrumb-nav">
             <div class="top-breadcrumb">
-            <!--div>
+                <div>
                     <ul class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Manage Shifts</a></li>
-                    <li class="breadcrumb-item"><a href="#">Shift List</a></li>
-                    <li class="breadcrumb-item">Add Shift </li>
-                    </ul> 
-                </div-->
+                        <li class="breadcrumb-item" style="color:#fff;">Inquiries</li>
+                    </ul>
+                </div>
 
             </div>
         </nav>
 
-        <div class="content">
-        <h2 class="table_topic">Inquiries</h2>
+        <div class="home-content">
+            <h2 class="table_topic">Inquiries</h2>
 
-        <input type="text" id="search" placeholder="Search by sender name.." title="senderName">
-       
+            <input type="text" id="search" placeholder="Search by sender name.." title="senderName">
 
-        <table style="width:90%;" class="table_view">
-            <thead>
-                <tr>
-                    <th>Sender Name</th>
-                    <th>Sender Email</th>
-                    <th>Inquiry</th>
-                    <th>Response</th>
-                    <th>Respond</th>
-                    <th>Remove</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Nethmi Pathirana</td>
-                    <td>nethmi.pathirana@gmail.com</td>
-                    <td>Is the pool open after 9 p.m on Friday?</td>
-                    <td>Unfortunately the pool will be closed after 7 p.m on Fridays for cleaning purposes.</td>
-                    <td><button class="button respond" id="myBtn">Respond</button></td>
-                    <td><button class="button remove">Delete</button></td>
-                </tr>
-                <tr>
-                    <td>Sandali Boteju</td>
-                    <td>sandali@yahoo.com</td>
-                    <td>Are there any packages available to book the entire sport's facility</td>
-                    <td>Kindly inquire us via the main line regarding this matter.</td>
-                    <td><button class="button respond" id="myBtn">Respond</button></td>
-                    <td><button class="button remove">Delete</button></td>
-                </tr>
-            </tbody>
-        </table>
+
+            <table style="width:90%;" class="table_view">
+                <thead>
+                    <tr>
+                        <th>Sender Name</th>
+                        <th>Sender Email</th>
+                        <th>Inquiry</th>
+                        <th>Response</th>
+                        <th>Respond</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nethmi Pathirana</td>
+                        <td>nethmi.pathirana@gmail.com</td>
+                        <td>Is the pool open after 9 p.m on Friday?</td>
+                        <td>Unfortunately the pool will be closed after 7 p.m on Fridays for cleaning purposes.</td>
+                        <td><button class="button respond" id="myBtn">Respond</button></td>
+                        <td><button class="button remove">Delete</button></td>
+                    </tr>
+                    <tr>
+                        <td>Sandali Boteju</td>
+                        <td>sandali@yahoo.com</td>
+                        <td>Are there any packages available to book the entire sport's facility</td>
+                        <td>Kindly inquire us via the main line regarding this matter.</td>
+                        <td><button class="button respond" id="myBtn">Respond</button></td>
+                        <td><button class="button remove">Delete</button></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </section>
 
@@ -119,7 +100,7 @@
                             </div>
                             <div class="form-group">
                                 <label for=""></label>
-                                <textarea rows="6" cols="53" name="response"  placeholder="Enter your response here"></textarea>
+                                <textarea rows="6" cols="53" name="response" placeholder="Enter your response here"></textarea>
                             </div>
                             </br>
                             <div class="form-group">
