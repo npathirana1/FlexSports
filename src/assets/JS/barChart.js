982a75f66f76f7d658a15e38da2a9ae81a6d8454
sandalib <2019is015@stu.ucsc.lk>
2022-01-10 16:16:47 +0530

    Basi of the PDF generation

--- src/assets/JS/barChart.js
+++ src/assets/JS/barChart.js
@@ -25,4 +25,42 @@ function drawChart() {
 
     chart.draw(data, google.charts.Bar.convertOptions(options));
 
+
+
 }
+/*
+google.charts.load("current", { packages: ['bar'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Facility', 'This Week', 'Last Week'],
+        ['Badminton', 2, 10],
+        ['Table tennis', 5, 14],
+        ['Basketball', 10, 5],
+        ['Billiards', 8, 12],
+        ['Vollyball', 1, 7],
+        ['Swimmimg pool', 5, 13]
+    ]);
+
+    var options = {
+        legend: { position: 'top' },
+        hAxis: {
+            format: 'decimal'
+        },
+        height: 400,
+        colors: ['#0F305B', '#9c9c9c']
+    };
+
+    var chart_div = document.getElementById('mybar');
+    var chart = new google.visualization.ColumnChart(chart_div);
+
+    // Wait for the chart to finish drawing before calling the getImageURI() method.
+    google.visualization.events.addListener(chart, 'ready', function() {
+        chart_div.innerHTML = '<img src="' + chart.getImageURI() + '">';
+        console.log(chart_div.outerHTML);
+    });
+
+    chart.draw(data, options);
+}*/
