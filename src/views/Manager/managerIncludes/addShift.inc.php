bf0d1b1681d15364fd15a621a23c731b820d735c
sandalib <2019is015@stu.ucsc.lk>
2022-03-22 22:14:28 +0530

    minor changers

--- src/views/Manager/managerIncludes/addShift.inc.php
+++ src/views/Manager/managerIncludes/addShift.inc.php
@@ -55,6 +55,56 @@ if (isset($_POST['submit'])) {
             window.location.href = '../viewShift.php';
         </script>";
     }
+} elseif (isset($_POST['update'])) {
+    echo "
+    <script>
+        var ID = sessionStorage.getItem('EmpId');
+        sessionStorage.removeItem('EmpId');
+        var Shift = sessionStorage.getItem('Shift');
+        sessionStorage.removeItem('Shift');
+        var Date = sessionStorage.getItem('Date');
+        sessionStorage.removeItem('Date');
+    </script>";
+    $preID = "<script>document.writeln(ID);</script>";
+    $preShift = "<script>document.writeln(Shift);</script>";
+    $preDate = "<script>document.writeln(Date);</script>";
+    echo $preID;
+    echo $preDate;
+
+    $newDate = date('Y-m-d', strtotime($_POST['date']));
+    $newShift = $_POST['shift'];
+    if (!empty($_POST['date']) && !empty($_POST['shift'])) {
+        $query1 ="SELECT * FROM emp_shift WHERE Date='$preDate' AND Shift='$preShift'";
+        $result1 = mysqli_query($conn, $query1);
+        if (mysqli_num_rows($result1) > 0) {
+            echo
+            "<script>
+            alert('The slot is already filled');
+            window.location.href='../viewShift.php';
+        </script>";
+        } else {
+            $query2 = "UPDATE emp_shift SET Shift= '$newShift' , Date= '$newDate' WHERE EmpID='$preID' AND Date='$preDate' AND Shift='$preShift' ";
+            $result2 = mysqli_query($conn, $query2);
+            if ($result2 ) {
+                echo "<script>
+                        alert('Shift successfully updated');
+                        window.location.href='../viewShift.php';
+                    </script>";
+            } else {
+                echo "<script>
+            alert('Action Failed');
+            window.location.href='../viewShift.php';
+        </script>";
+            }
+        }
+    } else {
+        echo
+        "<script>
+        alert('Empty fields');
+        window.location.href = '../viewShift.php';
+    </script>";
+    }
 }
+
 
 mysqli_close($conn);
--- src/views/Manager/managerIncludes/deleteCustomer.inc.php
+++ src/views/Manager/managerIncludes/deleteCustomer.inc.php
@@ -33,6 +33,36 @@ if (isset($_POST['submit'])) {
         echo "<script>alert('Could not delete the customer account');</script>";
         echo "<script>window.location.href = '../customerList.php';</script>";
     }
+}elseif (isset($_POST['remove'])) {
+
+    $NIC = $_POST['nic'];
+    $UserType = 'customer';
+
+    $fetch_query = "SELECT * FROM customer WHERE NIC='$NIC'";
+    $fetch_result = mysqli_query($conn, $fetch_query);
+    $fetch_row = mysqli_fetch_assoc($fetch_result);
+    $user_id = $fetch_row["UserID"];
+
+    $dlt_user = "DELETE FROM user_login WHERE ID='$user_id' ";
+    $dlt_user_result = mysqli_query($conn, $dlt_user);
+
+    if($dlt_user_result){
+        $query = "DELETE FROM customer WHERE NIC='$NIC' ";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            echo "<script>
+                alert('Customer account has been successfully deleted');
+                window.location.href='../customerList.php';
+            </script>";
+        } else {
+            echo "<script>alert('Deletion failed');</script>";
+            echo "<script>window.location.href = '../customerList.php';</script>";
+        }
+    }else{
+        echo "<script>alert('Could not delete the customer account');</script>";
+        echo "<script>window.location.href = '../customerList.php';</script>";
+    }
 }
 
 mysqli_close($conn);
